d2f50cd7dafc836f61d2c7fca0c0060107f47557
igorkcarneiro <igorcarneiros@pm.me>
2022-06-20 20:36:44 -0300

    modifications

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ide-frei <ide-frei@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 13:13:59 by ide-frei          #+#    #+#             */
-/*   Updated: 2022/06/20 14:41:40 by ide-frei         ###   ########.fr       */
+/*   Updated: 2022/06/20 20:34:33 by ide-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,9 +39,14 @@ static int ft_putnbr_u(unsigned int n)
 	return(ft_countdigits(aux));
 }
 
-hex_converter ()
+static void	*ft_hex_converter (int n)
 {
-	
+	if (n > 15)
+		ft_hex_converter(n / 16);
+	if (n % 16 < 10)
+		ft_putchar_fd(n % 16 + '0', 1);
+	else
+		ft_putchar_fd(n % 16 + 55, 1);
 }
 
 int	ft_printf(const char *beg, ...)
@@ -59,16 +64,18 @@ int	ft_printf(const char *beg, ...)
 				ft_putchar_fd(va_arg(list, int), 1);
 			if (beg[i + 1] == 's')
 				ft_putstr_fd(va_arg(list, char *), 1);
-			//if (beg[i + 1] == 'p')
-				
+			if (beg[i + 1] == 'p')
+				ft_hex_converter(va_arg(list, void *), 1);	
 			if (beg[i + 1] == 'd')
 				ft_putnbr_fd(va_arg(list, int), 1);
 			if (beg[i + 1] == 'i')
 				ft_putnbr_fd(va_arg(list, int), 1);
 			if (beg[i + 1] == 'u')
 				ft_putnbr_u(va_arg(list, int));
-			//if (beg[i + 1] == 'x')
-			//if (beg[i + 1] == 'X')
+			if (beg[i + 1] == 'x')
+				ft_hex_converter(ft_tolower(va_arg(list, void *)), 1);	
+			if (beg[i + 1] == 'X')
+				ft_hex_converter(va_arg(list, void *), 1);	
 			if (beg[i + 1] == '%')
 				ft_putchar_fd('%', 1);
 			i++;
@@ -84,8 +91,12 @@ int main (void)
 {
 	char c = 'c';
 	char *ch = "char";
+	void *p = 0xfafa;
 	int i = 12;
 	unsigned int y = 123;
+	int hex_l = 0x64;
+	int hex_u = 0xFAFA;
 	
 	ft_printf("char = %c\n string = %s\n intd = %d\n inti = %i\n unsigned = %u\n percent = %%\n", c, ch, i, i, y);
+	printf("char = %c\n string = %s\n intd = %d\n inti = %i\n unsigned = %u\n percent = %%\n", c, ch, i, i, y);
 }
