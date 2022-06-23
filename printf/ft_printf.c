7e471ad0658d3acc706b2070bf423d0cfc4a0fa4
igorcarneiros <igorcarneiros@pm.me>
2022-06-23 18:17:46 -0300

    modifications

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ide-frei <ide-frei@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 13:13:59 by ide-frei          #+#    #+#             */
-/*   Updated: 2022/06/20 20:34:33 by ide-frei         ###   ########.fr       */
+/*   Updated: 2022/06/22 19:21:23 by ide-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,64 +39,65 @@ static int ft_putnbr_u(unsigned int n)
 	return(ft_countdigits(aux));
 }
 
-static void	*ft_hex_converter (int n)
+static int	ft_hex_converter (unsigned long long n, int b)
 {
+	int i;
+
+	i = 0;
 	if (n > 15)
-		ft_hex_converter(n / 16);
+		ft_hex_converter(n / 16, b);
 	if (n % 16 < 10)
-		ft_putchar_fd(n % 16 + '0', 1);
+		i = ft_putchar_fd(n % 16 + '0', 1);
 	else
-		ft_putchar_fd(n % 16 + 55, 1);
+		i = ft_putchar_fd(n % 16 + b, 1);
+	return (i);
 }
 
 int	ft_printf(const char *beg, ...)
 {
 	va_list list;
 	int i;
+	int y;
 
 	i = 0;
+	y = 0;
 	va_start(list, beg);
 	while (beg[i])
 	{
 		if (beg[i] == '%' && ft_strchr("cspdiuxX%", beg[i + 1]))
 		{
 			if (beg[i + 1] == 'c')
-				ft_putchar_fd(va_arg(list, int), 1);
+				y += ft_putchar_fd(va_arg(list, int), 1);
 			if (beg[i + 1] == 's')
-				ft_putstr_fd(va_arg(list, char *), 1);
+				y += ft_putstr_fd(va_arg(list, char *), 1);
 			if (beg[i + 1] == 'p')
-				ft_hex_converter(va_arg(list, void *), 1);	
+				y += ft_hex_converter(va_arg(list, unsigned long long), 87);	
 			if (beg[i + 1] == 'd')
-				ft_putnbr_fd(va_arg(list, int), 1);
+				y += ft_putnbr_fd(va_arg(list, int), 1);
 			if (beg[i + 1] == 'i')
-				ft_putnbr_fd(va_arg(list, int), 1);
+				y += ft_putnbr_fd(va_arg(list, int), 1);
 			if (beg[i + 1] == 'u')
-				ft_putnbr_u(va_arg(list, int));
+				y += ft_putnbr_u(va_arg(list, int));
 			if (beg[i + 1] == 'x')
-				ft_hex_converter(ft_tolower(va_arg(list, void *)), 1);	
+				y += ft_hex_converter(va_arg(list, unsigned int), 87);	
 			if (beg[i + 1] == 'X')
-				ft_hex_converter(va_arg(list, void *), 1);	
+				y += ft_hex_converter(va_arg(list, unsigned int), 55);	
 			if (beg[i + 1] == '%')
-				ft_putchar_fd('%', 1);
+				y += ft_putchar_fd('%', 1);
 			i++;
 		}
 		else
-			ft_putchar_fd(beg[i], 1);
+			y += ft_putchar_fd(beg[i], 1);
 		i++;
 	}
-	return (i);
+	va_end(list);
+	return (y);
 }
 
+#include <stdio.h>
 int main (void)
 {
-	char c = 'c';
-	char *ch = "char";
-	void *p = 0xfafa;
-	int i = 12;
-	unsigned int y = 123;
-	int hex_l = 0x64;
-	int hex_u = 0xFAFA;
-	
-	ft_printf("char = %c\n string = %s\n intd = %d\n inti = %i\n unsigned = %u\n percent = %%\n", c, ch, i, i, y);
-	printf("char = %c\n string = %s\n intd = %d\n inti = %i\n unsigned = %u\n percent = %%\n", c, ch, i, i, y);
+	int i = 554;
+
+	ft_printf("%d\n", i);
 }
