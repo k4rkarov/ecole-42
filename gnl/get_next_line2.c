95b548e83e04c8e6184515f0b8f914a672cb724a
igorkcarneiro <igorcarneiros@pm.me>
2022-07-31 21:35:37 -0300

    2versions

--- gnl/get_next_line2.c
+++ gnl/get_next_line2.c
@@ -12,18 +12,45 @@
 
 #include "get_next_line.h"
 
-char	*put_new_line(char *line)
+char	*get_rest(char * line)
+{
+	char	*final;
+	int	count1;
+	int	count2;
+
+	count1 = 0;
+	count2 = 0;
+	while (line[count1] && line[count1] != '\n')
+		count1++;
+	if (!line[count1])
+	{
+		free(line);
+		return (NULL);
+	}
+	final = malloc((ft_strlen(line) - count1 + 1) * sizeof(char));
+	if (!final)
+		return (NULL);
+	count1++; //uso isso pra pular o \0.
+	while (line[count1])
+		final[count2++] = line[count1++];
+	final[count2] = '\0';
+	free(line);
+	return(final);
+}
+
+char	*get_break(char *line)
 {
 	char	*final;
 	int		count;
 
-	count = 0;
+	count = 0; //pra pegar o break
 	if (!line[count])
 		return (NULL);
 	while (line[count] && line[count] != '\n')
 		count++;
-	final = malloc(ft_strlen(line) - count + 2);
-	while (line[count])
+	final = malloc((count + 2) * sizeof(char));
+	count = 0; //pra botar as paradas dentro da variavel final
+	while (line[count] && line[count] != '\n')
 	{	
 		final[count] = line[count];
 		count++;
@@ -34,45 +61,13 @@ char	*put_new_line(char *line)
 	return (final);
 }
 
-char	*get_break(char *line)
-{
-	int		count1;
-	int		count2;
-	char	*bufferstr;
-	
-	count1 = 0;
-	count2 = 0;
-	while (line[count1] != '\n' && line[count1])
-		count1++;
-	if (!line[count1])
-	{
-		free(line);
-		return (NULL);
-	}
-	bufferstr = malloc(count1 + 1 * sizeof(char));
-	if (!bufferstr)
-		return (NULL);
-	count1 = 0;
-	while (line[count1] != '\n')
-		bufferstr[count2++] = line[count1++];
-	if (line[count1] == '\n')
-		bufferstr[count2++] = line[count1++]; 
-	bufferstr[count2] = '\0';
-	free(line);
-	return (bufferstr);
-}
-
 char	*get_line(int fd, char *line)
 {	
 	char	*buffer_endline;
 	int		br;
 	
 	br = 1;
-	if (!line)
-		line = ft_strdup("\0");
-	buffer_endline = (char *)malloc(BUFFER_SIZE + 1);
-	if (!buffer_endline)
-		return (NULL);
+	buffer_endline = malloc(BUFFER_SIZE + 1 * sizeof(char));
 	while (br != 0 && !ft_strchr(line, '\n'))
 	{
 		br = read(fd, buffer_endline, BUFFER_SIZE);
@@ -94,16 +89,16 @@ char	*get_next_line(int fd)
 	char *str_ret;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
+		return (0);
 	buffer_toread = get_line(fd, buffer_toread);
 	if (!buffer_toread)
 		return (NULL);
 	str_ret = get_break(buffer_toread);
-	buffer_toread = put_new_line(buffer_toread); 
+	buffer_toread = get_rest(buffer_toread); 
 	return (str_ret);
 }
 
-/*int main(void)
+/* int main(void)
 {
 	int	fd = open("example.txt", O_RDONLY);	
 	char *str = get_next_line(fd);
@@ -114,4 +109,4 @@ char	*get_next_line(int fd)
 		printf("%s", str);
 	
 	close(fd);
-}*/
+} */
