e4183a6e4dd5ac011cdf556712e275f188b4f4ab
igorkcarneiro <igorcarneiros@pm.me>
2023-02-06 18:33:26 -0300

    2 new functions

--- exam-rank-2/lvl1/repeat_alpha.c
+++ exam-rank-2/lvl1/repeat_alpha.c
@@ -2,15 +2,30 @@
 
 int main(int argc, char **argv)
 {
-	int i = 0;
+	int i = 0, y = 0;
+	char *str = argv[1];
 
 	if (argc == 2)
 	{
-		while(argv[1][i])
+		while(str[y])
 		{
-			write(1, &argv[1][i], sizeof(char));
-			i++;
+			if ((str[y] >= 'A' && str[y] <= 'Z') || (str[y] >= 'a' && str[y] <= 'z'))
+			{
+				if (str[y] >= 'a' && str[y] <= 'z')
+					i = (str[y] - 'a') + 1;
+				else if (str[y] >= 'A' && str[y] <= 'Z')
+					i = (str[y] - 'A') + 1;
+				while (i)
+				{
+					write(1, &str[y], 1);
+					i--;
+				}
+			}
+			else
+				write(1, &str[y], 1);
+			y++;
 		}
 	}
 	write(1, "\n", 1);
+	return (0);
 }
