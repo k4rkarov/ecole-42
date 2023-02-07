094b7f68f835b69baa3ae6973250102133d9818f
igorkcarneiro <igorcarneiros@pm.me>
2023-02-07 19:18:59 -0300

    new func lvl 1

--- exam-rank-2/lvl1/ulstr.c
+++ exam-rank-2/lvl1/ulstr.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int main (int argc, char ** argv)
 {
 	int i = 0;
 
@@ -9,12 +9,12 @@ int main(int argc, char **argv)
 		while (argv[1][i])
 		{
 			if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
-				argv[1][i] = (argv[1][i] - 'a' + 13) % 26 + 'a';
+				argv[1][i] = argv[1][i] - 32;
 			else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
-				argv[1][i] = (argv[1][i] - 'A' + 13) % 26 + 'A';
+				argv[1][i] = argv[1][i] + 32;
 			write(1, &argv[1][i], 1);
 			i++;
-		}	
+		}
 	}
 	write(1, "\n", 1);
 	return (0);
